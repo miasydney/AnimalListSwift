c85b923be9b28ecfba6995b34c2dd82ea3020bfa
mia <miabenn74@gmail.com>
2023-04-13 13:07:06 +1000

    initial commit

--- Zookeeper/ContentView.swift
+++ Zookeeper/ContentView.swift
@@ -8,15 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let animalGroups = [
+        AnimalGroup(groupName: "Pets", animals: [
+            Animal(name: "Dog"),
+            Animal(name: "Cat"),
+            Animal(name: "Parrot"),
+            Animal(name: "Mouse")
+        ]),
+        AnimalGroup(groupName: "Farm", animals: [
+            Animal(name: "Cow"),
+            Animal(name: "Horse"),
+            Animal(name: "Goat"),
+            Animal(name: "Sheep"),
+        ]),
+        AnimalGroup(groupName: "Critters", animals: [
+            Animal(name: "Firefly"),
+            Animal(name: "Spider"),
+            Animal(name: "Ant"),
+            Animal(name: "Squirrel"),
+        ])
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            ForEach(animalGroups) { animalGroup in
+                Section(header: Text(animalGroup.groupName)) {
+                    ForEach(animalGroup.animals) { animal in
+                        Text(animal.name)
+                    }
+                }
+            }
         }
-        .padding()
     }
+}
+
+struct Animal: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+struct AnimalGroup: Identifiable {
+    var id = UUID()
+    var groupName : String
+    var animals : [Animal]
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -24,3 +59,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
