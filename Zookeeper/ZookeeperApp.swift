dcb9ff29cbfc797be34a3fe39cc9835e667ae0c5
mia <miabenn74@gmail.com>
2023-04-13 11:57:09 +1000

    Initial Commit

---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Mia on 13/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
